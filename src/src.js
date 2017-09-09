17787e9ba39fc13d97a15f0d5a3baeb29b6fcfd6
LiuHui <1132787527@qq.com>
2017-09-09 11:28:17 +0800

    test the input string's length and throw Exception when necessary

--- src/src.js
+++ src/src.js
@@ -1,3 +1,45 @@
+var feeStandard = {
+    normal:
+        [
+            {begin:9,end:12,price:30},
+            {begin:12,end:18,price:50},
+            {begin:18,end:20,price:80},
+            {begin:20,end:22,price:60}
+        ],
+    weekend:
+        [
+            {begin:9,end:12,price:40},
+            {begin:12,end:18,price:50},
+            {begin:18,end:22,price:60}
+    ]
+};
+
+var orderList = [{date:'2016-06-02',time:'20:00~22:00',place:'A'}];
+
 function showName(name){
     return 'name:' + name;
+}
+function createOder(order) {
+    var data = order.split(' ');
+    try {
+        if(data.length === 4|| data.length === 5){
+            addOrderToList(data);
+            console.log(orderList)
+        }
+        else {
+            throw new CommonException();
+        }
+    }
+    catch (e){
+        console.log(e.message);
+        return(e.message);
+    }
+}
+
+function CommonException()
+{
+    this.message = 'Error: the booking is invalid';
+}
+function addOrderToList(order){
+    orderList.push({date:order[1],time:order[2],place:order[3]});
 }
--- src/test.js
+++ src/test.js
@@ -5,3 +5,10 @@ describe('just a test', function () {
         expect(result).toEqual(showName(data))
     })
 })
+describe('booking:if the input string not has the right length', function () {
+    it("test the input string's length" , function () {
+        var data = 'asd asdasdasd';
+        var result = 'Error: the booking is invalid';
+        expect(result).toEqual(createOder(data))
+    })
+})
